1ae4bb5226035a718bf9d437174ce7e50fa4fa66
Raman <raman89765038@gmail.com>
2025-07-24 23:50:27 +0530

    Adding

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import joblib
+import numpy as np
+from pydantic import BaseModel
+
+app = FastAPI()
+
+model = joblib.load("Decision.pkl")
+vectorizer = joblib.load("tfidf.pkl")
+
+label_map = {0: "figurative", 1: "irony", 2: "regular", 3: "sarcasm"}
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def get_form(request: Request):
+    return templates.TemplateResponse("form.html", {"request": request})
+
+@app.post("/predict/", response_class=HTMLResponse)
+async def predict(request: Request, text: str = Form(...)):
+    vector = vectorizer.transform([text])
+    prediction = model.predict(vector)[0]
+    label = label_map[int(prediction)]
+    return templates.TemplateResponse("form.html", {"request": request, "result": label})
